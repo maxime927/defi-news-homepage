9a3f60b3a9b9d8fff8984840eb415caa1849d75d
Maxime Ilambe <maxime.ilambe@gmail.com>
2022-11-10 15:01:49 +0100

    Updates desktop styles and fix details

--- www/templates/pages/home.php
+++ www/templates/pages/home.php
@@ -5,26 +5,39 @@
       <div class="flex-wrap">
         <div class="heading">
           <figure>
-            <img src="/dist/img/image-web-3-mobile.jpg" alt="" />
+            <picture>
+               <source
+                  media="(min-width: 1024px)"
+                  srcset="/dist/img/image-web-3-desktop.jpg">
+               <img src="/dist/img/image-web-3-mobile.jpg"
+               alt="a cute kitten">
+            </picture>
           </figure>
-          <h1>The Bright Future of Web 3.0</h1>
-          <p>We dive into the next evolutiion of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
-          <a class="btn btn-secondary">Read more</a>
+          <div class="infos">
+            <h1>The Bright Future of Web 3.0?</h1>
+            <div class="text">
+              <p>We dive into the next evolutiion of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+              <a class="btn btn-secondary" href="#">Read more</a>
+            </div>
+          </div>
         </div>
 
         <div class="news-bloc">
           <h2>New</h2>
           <ul>
             <li>
+              <a href="#" title="Hydrogen VS Electric Cars"></a>
               <h3>Hydrogen VS Electric Cars</h3>
               <p>Will hydrogen-fueled xars ever catch up to EVs?</p>
             </li>
             <li>
+              <a href="#" title="The Downsides of AI Artistry"></a>
               <h3>The Downsides of AI Artistry</h3>
               <p>What are the possible adverse effects of on-demand AI image generation?</p>
             </li>
             <li>
-              <h3>Is VC Funding Drying Up ?</h3>
+              <a href="#" title="Is VC Funding Drying Up?"></a>
+              <h3>Is VC Funding Drying Up?</h3>
               <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
             </li>
           </ul>
@@ -37,18 +50,27 @@
     <div class="container">
       <ul>
         <li class="card-news">
-          <a href="#" title=""></a>
-          <h3>Revivinng Retro PCs</h3>
-          <p>What happens when olddd PCs are given modern upgrades?</p>
+          <figure>
+            <img src="/dist/img/image-retro-pcs.jpg" alt="" />
+          </figure>
+          <a href="#" title="Revivinng Retro PCs"></a>
+          <div class="card-title">Reviving Retro PCs</div>
+          <p>What happens when old PCs are given modern upgrades?</p>
         </li>
         <li class="card-news">
-          <a href="#" title=""></a>
-          <h3>Top 10 Laptops of 2022</h3>
+          <figure>
+            <img src="/dist/img/image-top-laptops.jpg" alt="" />
+          </figure>
+          <a href="#" title="Top 10 Laptops of 2022"></a>
+          <div class="card-title">Top 10 Laptops of 2022</div>
           <p>Our best picks for various needs and budgets</p>
         </li>
         <li class="card-news">
-          <a href="#" title=""></a>
-          <h3>The Growth of Gaming</h3>
+          <figure>
+            <img src="/dist/img/image-gaming-growth.jpg" alt="" />
+          </figure>
+          <a href="#" title="The Growth of Gaming"></a>
+          <div class="card-title">The Growth of Gaming</div>
           <p>How the pandemix has sparked fresh opportunities</p>
         </li>
       </ul>
