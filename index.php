5191ae2ad096734aad3671a369697e7e53b83155
Maxime Ilambe <maxime.ilambe@gmail.com>
2022-11-11 01:14:26 +0100

    Cleaning files

--- index.php
+++ index.php
@@ -3,10 +3,6 @@ include 'www/functions.php';
 $data = grindXML('www/data.xml', true);
 $site = $settings->site;
 $is_responsive = (bool) (string) $site->isResponsive;
-if (isset($_GET['page']) && file_exists('www/templates/pages/'.$_GET['page'].'.php')) {
-    $page = $_GET['page'];
-}
-$include = isset($page) ? 'www/templates/pages/'.$page.'.php' : 'www/templates/__index.php';
 ?>
 <!doctype html>
 <html lang="fr">
@@ -32,13 +28,89 @@ $include = isset($page) ? 'www/templates/pages/'.$page.'.php' : 'www/templates/_
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-config" content="dist/img/favicons/browserconfig.xml">
 <meta name="theme-color" content="#ffffff"></head>
-<body class="page-<?=isset($page) ? ($page == 'home' ? 'home' : $page.' not-home') : 'template'?>">
+<body class="page-home">
   <!-- Header -->
   <?php include 'www/templates/_header.php'; ?>
   <!-- Main -->
-  <?php include $include; ?>
-  <!-- Footer -->
-  <?php include 'www/templates/_footer.php'; ?>
+  <div class="home-wrap">
+    <section class="first-bloc">
+      <div class="container">
+        <div class="flex-wrap">
+          <div class="heading">
+            <figure>
+              <picture>
+                 <source
+                    media="(min-width: 1024px)"
+                    srcset="/dist/img/image-web-3-desktop.jpg">
+                 <img src="/dist/img/image-web-3-mobile.jpg"
+                 alt="a cute kitten">
+              </picture>
+            </figure>
+            <div class="infos">
+              <h1>The Bright Future of Web 3.0?</h1>
+              <div class="text">
+                <p>We dive into the next evolutiion of the web that claims to put the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+                <a class="btn btn-secondary" href="#">Read more</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="news-bloc">
+            <h2>New</h2>
+            <ul>
+              <li>
+                <a href="#" title="Hydrogen VS Electric Cars"></a>
+                <h3>Hydrogen VS Electric Cars</h3>
+                <p>Will hydrogen-fueled xars ever catch up to EVs?</p>
+              </li>
+              <li>
+                <a href="#" title="The Downsides of AI Artistry"></a>
+                <h3>The Downsides of AI Artistry</h3>
+                <p>What are the possible adverse effects of on-demand AI image generation?</p>
+              </li>
+              <li>
+                <a href="#" title="Is VC Funding Drying Up?"></a>
+                <h3>Is VC Funding Drying Up?</h3>
+                <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="second-news-bloc">
+      <div class="container">
+        <ul>
+          <li class="card-news">
+            <figure>
+              <img src="/dist/img/image-retro-pcs.jpg" alt="" />
+            </figure>
+            <a href="#" title="Revivinng Retro PCs"></a>
+            <div class="card-title">Reviving Retro PCs</div>
+            <p>What happens when old PCs are given modern upgrades?</p>
+          </li>
+          <li class="card-news">
+            <figure>
+              <img src="/dist/img/image-top-laptops.jpg" alt="" />
+            </figure>
+            <a href="#" title="Top 10 Laptops of 2022"></a>
+            <div class="card-title">Top 10 Laptops of 2022</div>
+            <p>Our best picks for various needs and budgets</p>
+          </li>
+          <li class="card-news">
+            <figure>
+              <img src="/dist/img/image-gaming-growth.jpg" alt="" />
+            </figure>
+            <a href="#" title="The Growth of Gaming"></a>
+            <div class="card-title">The Growth of Gaming</div>
+            <p>How the pandemix has sparked fresh opportunities</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+
   <!-- Scripts -->
   <script src="dist/js/main.min.js"></script>
 </body>
--- www/templates/_footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer-wrap" role="contentinfo">
-  <div class="container">
-
-    <a id="back-to-top" href="#"><span class="visually-hidden">Retour en haut </span></a>
-  </div>
-</footer>
